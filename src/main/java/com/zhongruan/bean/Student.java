1482f5513c2d6af088519bc158ae1e0d9add4d2b
JiangXinWei-H <jxw961212@163.com>
2020-04-09 15:22:39 +0800

    修改过后的实体类

--- src/main/java/com/zhongruan/bean/Student.java
+++ src/main/java/com/zhongruan/bean/Student.java
@@ -5,8 +5,29 @@ public class Student {
     private int stuid;
     private String stuName;
 
+    public int getStuid() {
+        return stuid;
+    }
 
+    public void setStuid(int stuid) {
+        this.stuid = stuid;
+    }
 
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "stuid=" + stuid +
+                ", stuName='" + stuName + '\'' +
+                '}';
+    }
 }
 
 
